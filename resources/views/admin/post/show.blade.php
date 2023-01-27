6177026c676ec36f0a346c336d9bfbbcff2c8994
lollobianco <loreb2002@gmail.com>
2023-01-27 16:27:44 +0100

    Aggiunti tag nella show e nella index

--- resources/views/admin/post/show.blade.php
+++ resources/views/admin/post/show.blade.php
@@ -1,15 +1,22 @@
 @extends('layouts.app')
 
 @section('content')
+  <div class="container d-flex flex-column align-items-center text-white">
 
-    <div class="container d-flex flex-column align-items-center text-white">
+    <h2 class="my-4">{{ $post->title }}</h2>
 
-      <h2 class="mb-4">{{$post->title}}</h2>
+    <h5 class="mb-4">{{ $post->category['name'] }}</h5>
 
-      <h5 class="mb-4">{{$post->category['name']}}</h5>
-
-      <p>{{$post->body}}</p>
-
+    <div>
+      @foreach ($post->tags as $tag)
+        @if (is_null($tag->name))
+        @else
+          <span class="badge bg-light text-dark mb-4">{{ $tag['name'] }}</span>
+        @endif
+      @endforeach
     </div>
 
+    <p>{{ $post->body }}</p>
+
+  </div>
 @endsection
